89d65e70fab7b19ec64eef22c060c84df9778cc8
Hiral <hg1193@gmail.com>
2020-06-13 15:30:35 +0200

    initial commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+    class UserController extends AbstractController {
+        /**
+         * @Route("/")
+         * @Method({"GET"})
+         */
+        public function index() {
+            return $this->render('users/index.html.twig');
+        }
+    }
